e970962d7e395f92de4b1f09986bfb6680b55dc5
Keira Hodgkison <keira@cultureamp.com>
2017-07-08 16:12:23 +1000

    Update travis and server.js vars

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ const path = require('path');
 let app = express();
 let db;
 
-MongoClient.connect('mongodb://localhost/juniordevjobsdb', function(err, dbConnection) {
+let httpListenPort = process.env.PORT || 3000;
+let mongoDbUrl = process.env.MONGO_URL || 'mongodb://localhost/juniordevjobsdb';
+
+MongoClient.connect(mongoDbUrl, function(err, dbConnection) {
   db = dbConnection;
-  let server = app.listen(process.env.PORT || 3000, function() {
+  let server = app.listen(httpListenPort, function() {
 	  let port = server.address().port;
 	  console.log("Started server on port", port);
   });
